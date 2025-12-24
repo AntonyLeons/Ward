555637e531147e0b731440fb186d132457c17a7d
AntonyLeons <antony@leons.dev>
2025-12-24 20:14:01 +0000

    feat: add application access URL logging and native access support
    - Add logging of application access URLs including local and network addresses
    - Enable native access in JVM arguments for better performance
    - Disable SSL/TLS by default in configuration

--- src/main/java/dev/leons/ward/Ward.java
+++ src/main/java/dev/leons/ward/Ward.java
@@ -2,13 +2,18 @@ package dev.leons.ward;
 
 import dev.leons.ward.services.SetupService;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.ini4j.Ini;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Ward is a Spring Boot application class
@@ -16,6 +21,7 @@ import java.io.File;
  * @author Rudolf Barbu
  * @version 1.0.4
  */
+@Slf4j
 @SpringBootApplication
 public class Ward extends SpringBootServletInitializer {
     /**
@@ -48,6 +54,7 @@ public class Ward extends SpringBootServletInitializer {
 
         isFirstLaunch = true;
         configurableApplicationContext = SpringApplication.run(Ward.class, args);
+        logAccessUrls(configurableApplicationContext);
 
         File setupFile = new File(Ward.SETUP_FILE_PATH);
 
@@ -69,9 +76,48 @@ public class Ward extends SpringBootServletInitializer {
         {
             configurableApplicationContext.close();
             configurableApplicationContext = SpringApplication.run(Ward.class, args.getSourceArgs());
+            logAccessUrls(configurableApplicationContext);
         });
 
         thread.setDaemon(false);
         thread.start();
+    }
+
+    /**
+     * Logs the access URLs for the application
+     *
+     * @param context Application context
+     */
+    private static void logAccessUrls(ConfigurableApplicationContext context) {
+        String port = String.valueOf(INITIAL_PORT);
+        try {
+            File setupFile = new File(SETUP_FILE_PATH);
+            if (setupFile.exists()) {
+                Ini ini = new Ini(setupFile);
+                String configuredPort = ini.get("setup", "port");
+                if (configuredPort != null) {
+                    port = configuredPort;
+                }
+            }
+        } catch (Exception e) {
+            log.warn("Could not read setup.ini for port, using default");
+        }
+
+        String protocol = "http";
+        String localAddress = "localhost";
+        String networkAddress = "127.0.0.1";
+        try {
+            networkAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application Ward is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://{}:{}\n\t" +
+                        "Network: \t{}://{}:{}\n" +
+                        "----------------------------------------------------------",
+                protocol, localAddress, port,
+                protocol, networkAddress, port);
     }
 }
